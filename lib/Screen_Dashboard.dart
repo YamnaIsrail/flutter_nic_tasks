2e5ddbf51727d572df2438f85ff71083174e4ebc
Yum~ <yamnaisrailkhan@gmail.com>
2024-01-31 13:05:35 +0500

    Prayer Times API implemented in app

--- lib/Screen_Dashboard.dart
+++ lib/Screen_Dashboard.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nic_flutter_assignments/PrayerApiAssignment/api_screen_date_selection.dart';
+
 
 class ScreenDashboard extends StatefulWidget {
   const ScreenDashboard({super.key});
@@ -10,6 +12,28 @@ class ScreenDashboard extends StatefulWidget {
 class _ScreenDashboardState extends State<ScreenDashboard> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard", style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,),
+
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          ListTile(
+              title: Text("Prayer Time API "),
+              onTap: (){
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => DateSelectionPage()));
+              },),
+          ListTile(title: Text("SQFLite Database"),),
+          ListTile(title: Text("Hive Database"),),
+          ListTile(title: Text(""),),
+
+        ],
+
+      ),
+    );
   }
 }
