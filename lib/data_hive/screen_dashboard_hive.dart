a122cf48a6923df50604d2d773305c67c127af25
Yum~ <yamnaisrailkhan@gmail.com>
2024-01-31 22:09:37 +0500

    Screen Dashboard for Hive Database

---
+++ lib/data_hive/screen_dashboard_hive.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class hiveDashboard extends StatefulWidget {
+  const hiveDashboard({super.key});
+
+  @override
+  State<hiveDashboard> createState() => _hiveDashboardState();
+}
+
+class _hiveDashboardState extends State<hiveDashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
